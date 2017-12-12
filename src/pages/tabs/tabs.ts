c037a8bcfc6c0d2b68bd0ca6f6f9a03d73e982af
Francesco <francesco.falchi@gmail.com>
2017-12-12 11:35:14 +0100

    add calculator tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { CalculatorPage } from '../calculator/calculator'
 import { AboutPage } from '../about/about';
 
 @Component({
@@ -7,7 +8,8 @@ import { AboutPage } from '../about/about';
 })
 export class TabsPage {
 
-  tab2Root = AboutPage;
+  calculatorTab = CalculatorPage;
+  aboutTab = AboutPage;
 
   constructor() {
 
