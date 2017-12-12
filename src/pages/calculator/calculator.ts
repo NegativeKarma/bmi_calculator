d15e0c4a2a2e7f49502031415f05ec5fa84a7911
Francesco <francesco.falchi@gmail.com>
2017-12-12 21:53:17 +0100

    Add imperial calculations feature

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -12,6 +12,7 @@ export class CalculatorPage {
   weight: number;
   bmiValue: number;
   bmiMessage: string;
+  systemType: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -23,6 +24,14 @@ export class CalculatorPage {
   calculateBMI() {
     if (this.weight > 0 && this.height > 0 ) {
       let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+
+  calculateBMI2() {
+    if (this.weight > 0 && this.height > 0 ) {
+      let finalBmi = (this.weight * 703) / (this.height * this.height);
       this.bmiValue = parseFloat(finalBmi.toFixed(2));
       this.setBMIMessage();
     }
